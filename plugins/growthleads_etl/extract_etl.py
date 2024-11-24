faae9d358cb856458cea4692e90c7bd734733038
Louis <97873724+LouisYC123@users.noreply.github.com>
2024-11-24 16:56:37 +0000

    added source data standardisation functions

--- plugins/growthleads_etl/extract_etl.py
+++ plugins/growthleads_etl/extract_etl.py
@@ -1,14 +1,15 @@
 import os
 import pandas as pd
 import re
-from typing import Literal
 from pathlib import Path
 import glob
+from datetime import datetime
+import shutil
 
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 
-DATA_SOURCES = Literal["routy", "voluum", "manual"]
-SCHEMA_NAMES = Literal["bronze", "silver", "gold"]
+from growthleads_etl.config import SCHEMA_NAMES, LOAD_TYPES
+import hashlib
 
 
 # utils
@@ -28,21 +29,66 @@ def is_csv_available(folder_path: str):
     return False
 
 
-def standardise_column_names(df: pd.DataFrame) -> pd.DataFrame:
-    """Ensures all column names in a DataFrame are snake_case"""
-    df.columns = [re.sub(r"(?<!^)(?=[A-Z])", "_", col).lower() for col in df.columns]
-    return df
+def move_csv_to_archive(source: str, landing_zone: Path, archive_zone: Path):
+    """Move CSV files from the landing zone to the archive folder."""
+
+    landing_path = landing_zone / source
+    archive_path = archive_zone / source
+
+    os.makedirs(archive_path, exist_ok=True)
+
+    for file in landing_path.glob("*.csv"):
+        timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
+        archive_file = archive_path / f"{file.stem}_{timestamp}{file.suffix}"
+        shutil.move(file, archive_file)
+        print(f"Moved {file} to {archive_file}")
 
 
-def convert_integers_to_floats(df: pd.DataFrame) -> pd.DataFrame:
+def standardise_number_formats(df: pd.DataFrame) -> pd.DataFrame:
     """Standardise all numerical values as floats"""
     for col in df.select_dtypes(include="int").columns:
         df[col] = df[col].astype(float)
     return df
 
 
+def standardise_column_names(df: pd.DataFrame) -> pd.DataFrame:
+    """Ensures all column names in a DataFrame are snake_case"""
+    df.columns = [re.sub(r"(?<!^)(?=[A-Z])", "_", col).lower() for col in df.columns]
+    return df
+
+
+def standardise_dates(df: pd.DataFrame) -> pd.DataFrame:
+    if "date" in df.columns:
+        df["date"] = pd.to_datetime(
+            df["date"], errors="coerce", infer_datetime_format=True
+        )
+        df["date"] = df["date"].dt.strftime(
+            "%Y-%m-%d"
+        )  # Standardise to 'YYYY-MM-DD' format
+    return df
+
+
+def standardise_dataframe(df: pd.DataFrame) -> pd.DataFrame:
+    """Standardise the DataFrame by applying all standardisation functions."""
+    return (
+        df.pipe(standardise_column_names)
+        .pipe(standardise_dates)
+        .pipe(standardise_number_formats)
+    )
+
+
+def add_metadata(df: pd.DataFrame, source: str, filename: str) -> pd.DataFrame:
+    """Add metadata columns to the DataFrame."""
+    return df.assign(
+        source=source,
+        filename=filename,
+        ingestion_timestamp=pd.Timestamp.now(),
+        source_id=hashlib.md5(f"{source}{filename}".encode()).hexdigest(),
+    )
+
+
 # readers
-def read_data(source: DATA_SOURCES, data_dir: Path):
+def read_data(source: str, data_dir: Path):
     """Read data from a source and return a DataFrame with added metadata."""
     print(f"Reading {source} data...")
 
@@ -55,12 +101,8 @@ def read_data(source: DATA_SOURCES, data_dir: Path):
     return pd.concat(
         [
             pd.read_csv(file)
-            .pipe(standardise_column_names)
-            .pipe(convert_integers_to_floats)
-            .assign(date=lambda df: pd.to_datetime(df["date"], errors="coerce"))
-            .assign(source=source)
-            .assign(filename=Path(file).name)
-            .assign(ingestion_timestamp=pd.Timestamp.now())
+            .pipe(standardise_dataframe)
+            .pipe(add_metadata, source, Path(file).name)
             for file in all_files
         ],
         ignore_index=True,
@@ -73,7 +115,7 @@ def load_dataframe_to_postgres(
     table_name: str,
     schema_name: SCHEMA_NAMES,
     postgres_conn_id: str,
-    if_exists: Literal["append", "fail", "replace"] = "append",
+    if_exists: LOAD_TYPES,
 ):
     """Load a Pandas DataFrame to a PostgreSQL table using pd.to_sql()"""
     pg_hook = PostgresHook(postgres_conn_id=postgres_conn_id)
