ac6e14b163ba3233c27b1f41b6f9e7ff8941ad55
Louis <97873724+LouisYC123@users.noreply.github.com>
2024-11-24 16:57:35 +0000

    added extract and transform taskgroups

--- plugins/growthleads_etl/tasks.py
+++ plugins/growthleads_etl/tasks.py
@@ -1,39 +1,48 @@
 import pandas as pd
 
-
-from growthleads_etl import extract_etl, tasks
-from growthleads_etl.settings import LANDING_ZONE, POSTGRES_CONN_ID
+from growthleads_etl import extract_etl
+from growthleads_etl import config
 
 from airflow.decorators import task
 from airflow.utils.task_group import TaskGroup
 from airflow.sensors.python import PythonSensor
+from cosmos import DbtTaskGroup, ProjectConfig, ProfileConfig
 
 
 @task
-def read_data_task(source: str, landing_zone: str):
+def read_data(source: str):
     """Read data from the source and return a DataFrame."""
-    return extract_etl.read_data(source, landing_zone)
+    return extract_etl.read_data(source, config.LANDING_ZONE)
 
 
 @task
-def validate_schema_task(df: pd.DataFrame, schema):
+def validate_schema(df: pd.DataFrame, schema):
     """Validate the DataFrame against the defined schema."""
     return schema.validate(df, lazy=True)
 
 
 @task
-def load_data_task(df: pd.DataFrame, source: str, layer: str, conn_id: str):
+def load_to_db(df: pd.DataFrame, source: str, layer: str, load_type: config.LOAD_TYPES):
     """Load the DataFrame to the target database."""
-    return extract_etl.load_dataframe_to_postgres(df, source, layer, conn_id)
+    return extract_etl.load_dataframe_to_postgres(
+        df, source, layer, config.POSTGRES_CONN_ID, if_exists=load_type
+    )
 
 
-def extract_source(source: str, schema):
+@task
+def archive_data(source: str):
+    """Archive the CSV files after processing."""
+    return extract_etl.move_csv_to_archive(
+        source, config.LANDING_ZONE, config.ARCHIVE_ZONE
+    )
+
+
+def extract_source(source: str, source_config: str):
     """
     Create a TaskGroup for extracting and processing data from a specific source.
 
     Args:
         source (str): The name of the data source (e.g., 'routy').
-        schema: The validation schema object.
 
     Returns:
         TaskGroup: A TaskGroup object containing all tasks for the source.
@@ -43,29 +52,67 @@ def extract_source(source: str, schema):
         wait_for_csv = PythonSensor(
             task_id=f"wait_for_{source}_csv",
             python_callable=extract_etl.is_csv_available,
-            op_args=[str(LANDING_ZONE / source)],  # Convert path to string
-            poke_interval=5,
-            timeout=21600,  # Timeout after 6 hours
+            op_args=[str(config.LANDING_ZONE / source)],
+            poke_interval=config.LANDING_POKE_INTERVAL,
+            timeout=config.LANDING_TIMEOUT,
         )
 
         # Task 2: Read Data
-        data = tasks.read_data_task(source=source, landing_zone=LANDING_ZONE)
+        data = read_data(source)
 
         # Task 3: Validate Schema
-        validated_data = validate_schema_task(
+        validated_data = validate_schema(
             df=data,
-            schema=schema,
+            schema=source_config["schema"],
         )
 
         # Task 4: Load Data to db
-        load_data = load_data_task(
+        load_data = load_to_db(
             df=validated_data,
             source=source,
             layer="bronze",
-            conn_id=POSTGRES_CONN_ID,
+            load_type=source_config["load_type"],
         )
 
-        # Set dependencies
+        # Conditionally add Task 5: Archive Data
+        if source_config.get("archive", False):
+            archive = archive_data(source)
+            load_data >> archive
+
+        # Set initial dependencies
         wait_for_csv >> data >> validated_data >> load_data
 
     return extract_group
+
+
+def extract_taskgroup(data_sources: dict, group_id: str):
+    """
+    Dynamically creates tasks for each data source in the config and adds them to a TaskGroup.
+
+    Args:
+        config: A dictionary of data sources.
+        tasks: A module containing reusable task functions.
+
+    Returns:
+        TaskGroup: The dynamically generated TaskGroup.
+    """
+    with TaskGroup(group_id=group_id) as extract:
+        for source_name, source_config in data_sources.items():
+            source_task_group = extract_source(source_name, source_config)
+            source_task_group
+    return extract
+
+
+def transform_taskgroup(dbt_config: dict):
+
+    with DbtTaskGroup(
+        project_config=ProjectConfig(dbt_config["dbt_root_path"]),
+        profile_config=ProfileConfig(
+            profiles_yml_filepath=dbt_config["dbt_profiles_dir"],
+            profile_name=dbt_config["dbt_profile_name"],
+            target_name=dbt_config["dbt_target_name"],
+        ),
+    ) as transform_group:
+        pass
+
+    return transform_group
