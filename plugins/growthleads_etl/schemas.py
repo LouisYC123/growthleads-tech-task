543aa0c94bf1dec278d13f83fa0aa9e6a22c8597
Louis <97873724+LouisYC123@users.noreply.github.com>
2024-11-24 16:56:56 +0000

    added remaining pandera source data schemas

--- plugins/growthleads_etl/schemas.py
+++ plugins/growthleads_etl/schemas.py
@@ -1,30 +1,57 @@
-from pandera import Column, DataFrameSchema
-import pandera as pa
+from pandera import Field, DataFrameModel
+from functools import partial
+import pandas as pd
+
+Coerced = partial(Field, coerce=True)
+Unique = partial(Coerced, unique=True)
+Nullable = partial(Coerced, nullable=True)
 
 
-RoutyBronzeDataset = DataFrameSchema(
-    {
-        "date": Column(pa.DateTime, nullable=True),
-        "marketing_source": Column(pa.String, nullable=True),
-        "operator": Column(pa.String, nullable=True),
-        "country": Column(pa.String, nullable=True),
-        "country_code": Column(pa.String, nullable=True),
-        "raw_earnings": Column(pa.Float, nullable=True),
-        "visits": Column(pa.Float, nullable=True),
-        "signups": Column(pa.Float, nullable=True),
-        "source": Column(pa.String, nullable=True),
-        "filename": Column(pa.String, nullable=True),
-        "ingestion_timestamp": Column(pa.Timestamp, nullable=True),
-    }
-)
+class BaseBronzeSchema(DataFrameModel):
+    source: pd.StringDtype = Coerced()
+    filename: pd.StringDtype = Coerced()
+    ingestion_timestamp: pd.Timestamp = Coerced()
+    source_id: pd.StringDtype = Coerced()
 
-VoluumBronzeDataset = DataFrameSchema(
-    {
-        "date": Column(pa.DateTime, nullable=True),
-        "voluum_brand": Column(pa.String, nullable=True),
-        "clicks": Column(pa.Float, nullable=True),
-        "source": Column(pa.String, nullable=True),
-        "filename": Column(pa.String, nullable=True),
-        "ingestion_timestamp": Column(pa.Timestamp, nullable=True),
-    }
-)
+
+class RoutyBronzeDataset(BaseBronzeSchema):
+    date: pd.Timestamp = Nullable()
+    marketing_source: pd.StringDtype = Nullable()
+    operator: pd.StringDtype = Nullable()
+    country: pd.StringDtype = Nullable()
+    country_code: pd.StringDtype = Nullable()
+    raw_earnings: pd.Float64Dtype = Nullable()
+    visits: pd.Float64Dtype = Nullable()
+    signups: pd.Float64Dtype = Nullable()
+
+
+class VoluumBronzeDataset(BaseBronzeSchema):
+    date: pd.Timestamp = Nullable()
+    voluum_brand: pd.StringDtype = Nullable()
+    clicks: pd.Float64Dtype = Nullable()
+
+
+class ManualBronzeDataset(BaseBronzeSchema):
+    date: pd.Timestamp = Nullable()
+    marketing_source: pd.StringDtype = Nullable()
+    operator: pd.StringDtype = Nullable()
+    raw_earnings: pd.Float64Dtype = Nullable()
+    visits: pd.Float64Dtype = Nullable()
+    signups: pd.Float64Dtype = Nullable()
+
+
+class DealsBronzeDataset(BaseBronzeSchema):
+    marketing_source: pd.StringDtype = Nullable()
+    deal: pd.StringDtype = Nullable()
+    comments: pd.StringDtype = Nullable()
+
+
+class VoluumMapperBronzeDataset(BaseBronzeSchema):
+    voluum_brand: pd.StringDtype = Nullable()
+    marketing_source: pd.StringDtype = Nullable()
+
+
+class CentralMappingBronzeDataset(BaseBronzeSchema):
+    variants: pd.StringDtype = Nullable()
+    std_operator: pd.StringDtype = Nullable()
+    country: pd.StringDtype = Nullable()
