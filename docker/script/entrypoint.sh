32047f3a2ad0be58f13caa98e84bc9d1742532f5
Louis <97873724+LouisYC123@users.noreply.github.com>
2024-11-29 09:34:38 +0000

    removed startup script

--- docker/script/entrypoint.sh
+++ docker/script/entrypoint.sh
@@ -62,18 +62,6 @@ wait_for_port() {
   done
 }
 
-execute_startup_script() {
-  # Execute customer provided shell script
-  if [[ -e "$AIRFLOW_HOME/startup/startup.sh" ]]; then
-    bash /shell-launch-script.sh
-    source stored_env
-    export AIRFLOW_HOME="/usr/local/airflow"
-    export AIRFLOW__CORE__LOAD_EXAMPLES="False"
-    cd "$AIRFLOW_HOME"
-  else
-    echo "No startup script found, skipping execution."
-  fi
-}
 
 # Other executors than SequentialExecutor drive the need for an SQL database, here PostgreSQL is used
 if [ "$AIRFLOW__CORE__EXECUTOR" != "SequentialExecutor" ]; then
@@ -126,7 +114,6 @@ case "$1" in
     fi
 
 
-    execute_startup_script
     source stored_env
     export AIRFLOW_HOME="/usr/local/airflow"
     export AIRFLOW__CORE__LOAD_EXAMPLES="False"
@@ -164,9 +151,6 @@ case "$1" in
       aws s3 cp $S3_REQUIREMENTS_PATH $AIRFLOW_HOME/$REQUIREMENTS_FILE
     fi      
     package_requirements
-    ;;
-  test-startup-script)
-    execute_startup_script
     ;;
   *)
     # The command is something like bash, not an airflow subcommand. Just run it in the right environment.
